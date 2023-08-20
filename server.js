9ec2e5799995912e537ce144b7970e24f1a8e9b4
UmamaheshMaxwell <devopswithkube@gmail.com>
2023-08-20 18:44:14 +0530

    added http2 module

--- server.js
+++ server.js
@@ -1,33 +1,54 @@
-const express = require('express');
-const spdy = require('spdy');
-const fs = require('fs');
-const adminRouter = require("./routes/admin.router")
+// const express = require('express');
+// const http2 = require('http2');
+// const fs = require('fs');
+// const adminRouter = require('./routes/admin.router');
 
-const PORT = 8080;
+// const PORT = 8080;
+// const CERT_DIR = `${__dirname}/cert`;
+// const useSSL = !!process.env.SSL;
+
+// const app = express();
+
+// app.use('/api', adminRouter);
+
+// const serverOptions = {
+//   key: fs.readFileSync(`${CERT_DIR}/server.key`),
+//   cert: fs.readFileSync(`${CERT_DIR}/server.cert`),
+//   allowHTTP1: true
+// };
+
+// const server = useSSL
+//   ? http2.createSecureServer(serverOptions, app)
+//   : http2.createServer(app);
+
+// server.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}`);
+//   console.log(`SSL ${useSSL ? 'enabled' : 'disabled'}`);
+// });
+
+const http2 = require('node:http2');
+const fs = require('node:fs');
+
 const CERT_DIR = `${__dirname}/cert`;
 const useSSL = !!process.env.SSL;
+const PORT = 8080;
 
-const app = express();
+const server = http2.createSecureServer({
+    key: fs.readFileSync(`${CERT_DIR}/server.key`),
+    cert: fs.readFileSync(`${CERT_DIR}/server.cert`),
+});
+server.on('error', (err) => console.error(err));
 
-app.use("/api", adminRouter)
-
-function createServer() {
-    if (!useSSL) {
-        return app;
-    }
-    return spdy.createServer(
-        {
-            key: fs.readFileSync(`${CERT_DIR}/server.key`),
-            cert: fs.readFileSync(`${CERT_DIR}/server.cert`),
-            allowHTTP1: true
-        },
-        app
-    );
-}
-
-const server = createServer();
+server.on('stream', (stream, headers) => {
+  // stream is a Duplex
+  stream.respond({
+    'content-type': 'text/html; charset=utf-8',
+    ':status': 200,
+  });
+  stream.end('<h1>Hello World</h1>');
+});
 
 server.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
     console.log(`SSL ${useSSL ? 'enabled' : 'disabled'}`);
-});
+  });
