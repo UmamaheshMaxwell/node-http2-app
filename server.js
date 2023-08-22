3d766481296c94e9dbd2b56ab6936b7f4c0a32ea
UmamaheshMaxwell <devopswithkube@gmail.com>
2023-08-22 23:18:42 +0530

    added server

--- server.js
+++ server.js
@@ -1,54 +1,45 @@
-const express = require('express');
-const spdy = require('spdy');
-const fs = require('fs');
-
-const PORT = 8080;
-const CERT_DIR = `${__dirname}/cert`;
-
-const app = express();
-
-app.get('/', (_, res) => {
-  res.send('hello world');
-});
-
-const server = spdy.createServer(
-  {
-    key: fs.readFileSync(`${CERT_DIR}/server.key`),
-    cert: fs.readFileSync(`${CERT_DIR}/server.cert`),
-  },
-  app
-);
-
-//createServer();
-
-server.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('SSL Enabled');
-});
 
 // const http2 = require('http2');
 // const fs = require('fs');
 
-// const CERT_DIR = `${__dirname}/cert`;
-// const useSSL = !!process.env.SSL;
-// const PORT = 8080;
-
-// const server = http2.createSecureServer({
-//     key: fs.readFileSync(`${CERT_DIR}/server.key`),
-//     cert: fs.readFileSync(`${CERT_DIR}/server.cert`),
-// });
-// server.on('error', (err) => console.error(err));
+// const server = http2.createServer();
 
 // server.on('stream', (stream, headers) => {
-//   // stream is a Duplex
+//   // Handle the incoming request stream here
 //   stream.respond({
-//     'content-type': 'text/html; charset=utf-8',
 //     ':status': 200,
+//     'content-type': 'text/plain'
 //   });
-//   stream.end('<h1>Hello World</h1>');
+//   stream.end('Hello, HTTP/2 cleartext (h2c) world!\n');
 // });
 
-// server.listen(PORT, () => {
-//     console.log(`App listening on port ${PORT}`);
-//     console.log(`SSL ${useSSL ? 'enabled' : 'disabled'}`);
-//   });
+// server.listen(3000, () => {
+//   console.log('HTTP/2 cleartext server is listening on port 3000');
+// });
+
+const http2 = require('http2');
+const fs = require('fs');
+
+const CERT_DIR = `${__dirname}/cert`;
+const useSSL = !!process.env.SSL;
+const PORT = 8080;
+
+const server = http2.createSecureServer({
+    key: fs.readFileSync(`${CERT_DIR}/server.key`),
+    cert: fs.readFileSync(`${CERT_DIR}/server.cert`),
+});
+server.on('error', (err) => console.error(err));
+
+server.on('stream', (stream, headers) => {
+  // stream is a Duplex
+  stream.respond({
+    'content-type': 'text/html; charset=utf-8',
+    ':status': 200,
+  });
+  stream.end('<h1>Hello World</h1>');
+});
+
+server.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log(`SSL ${useSSL ? 'enabled' : 'disabled'}`);
+  });
