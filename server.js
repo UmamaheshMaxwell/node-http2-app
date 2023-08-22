c931d5d080cc6793c29280ba3aaf8464bb9c9886
UmamaheshMaxwell <devopswithkube@gmail.com>
2023-08-23 01:25:08 +0530

    added cert

--- server.js
+++ server.js
@@ -1,45 +1,24 @@
-// const express = require('express');
-// const spdy = require('spdy');
-// const fs = require('fs');
-// const adminRouter = require("./routes/admin.router")
+const http2 = require ("http2")
+const fs = require("fs")
 
-// const PORT = 8080;
-// const CERT_DIR = `${__dirname}/cert`;
-// const useSSL = !!process.env.SSL;
+const server = http2.createSecureServer({
+    "key": fs.readFileSync("localhost-private.pem"),
+    "cert": fs.readFileSync("localhost-cert.pem")
+})
 
-// const app = express();
+server.on("stream", (stream, headers) => {
+    console.log(stream.id);
+    stream.respond({
+        "content-type": "application/json",
+        "status": 200
+    })
 
-// app.use("/api", adminRouter)
+    stream.end(JSON.stringify({
+        "user": "Umamahesh",
+        "id": 1
+    }))
+})
 
-// function createServer() {
-//     if (!useSSL) {
-//         return app;
-//     }
-//     return spdy.createServer(
-//         {
-//             key: fs.readFileSync(`${CERT_DIR}/server.key`),
-//             cert: fs.readFileSync(`${CERT_DIR}/server.cert`)
-//         },
-//         app
-//     );
-// }
 
-// const server = createServer();
-
-// server.listen(PORT, () => {
-//     console.log(`App listening on port ${PORT}`);
-//     console.log(`SSL ${useSSL ? 'enabled' : 'disabled'}`);
-// });
-
-const express = require('express');
-const adminRouter = require("./routes/admin.router")
-
-const PORT = process.env.PORT || 8080;
-
-const app = express();
-
-app.use("/api", adminRouter)
-
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
-});
+server.listen(8080);
+console.log("listening on port 8080");
