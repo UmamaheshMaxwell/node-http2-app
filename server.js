5528fd81709399fb6455fa5d96a3db3b6d3b566e
UmamaheshMaxwell <devopswithkube@gmail.com>
2023-08-28 14:15:36 +0530

    added winston for logs

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const express = require('express')
+const express = require('express');
+const router = express.Router();
 const http2Express = require('http2-express-bridge')
 const http2 = require('http2')
 const fs = require('fs')
+const winston = require("winston")
+const users = require("./users.json")
+
 const adminRouter = require('./routes/admin.router');
 
 const app = http2Express(express)
@@ -17,8 +21,35 @@ const options = {
     rejectUnauthorized: false 
 };
 
+const logger = winston.createLogger({
+    level: "info", // Set log level
+    format: winston.format.json(), // Use JSON format
+    defaultMeta: { service: "node-http2-app" }, // Add metadata
+    transports: [
+        new winston.transports.Console(), // Log to console
+    ],
+});
 
-app.use('/api', adminRouter);
+router.get("/admin", async (req, res) => {  
+    try {
+        res.json({ message: "Welcome to HTTP2 API" });
+        logger.info("Request handled successfully");
+    } catch (error) {
+        logger.error("Error in /admin route", { error });
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+router.get("/users", (req, res) => {
+    try {
+        res.json(users);
+    } catch (error) {
+        console.error("Error in /users route:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.use('/api', router);
 
 const server = http2.createSecureServer(options, app)
 
