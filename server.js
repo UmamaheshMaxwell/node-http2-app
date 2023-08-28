cb9fc6c3a4102e5257b74bc0cc97d72a521ea304
UmamaheshMaxwell <devopswithkube@gmail.com>
2023-08-28 14:43:24 +0530

    modified code

--- server.js
+++ server.js
@@ -1,59 +1,30 @@
-const express = require('express');
-const router = express.Router();
-const http2Express = require('http2-express-bridge')
-const http2 = require('http2')
+const spdy = require('spdy')
+const express = require('express')
+const path = require('path')
 const fs = require('fs')
-const winston = require("winston")
-const users = require("./users.json")
 
-const adminRouter = require('./routes/admin.router');
-
-const app = http2Express(express)
-
-const PORT = 443;
+const port = 8080
 const CERT_DIR = `${__dirname}/cert`;
-const useSSL = !!process.env.SSL;
+const app = express()
 
+app.get('*', (req, res) => {
+  res
+    .status(200)
+    .json({message: 'Welcome to node HTTP2'})
+})
 const options = {
     key: fs.readFileSync(`${CERT_DIR}/server.key`),
     cert: fs.readFileSync(`${CERT_DIR}/server.crt`),
     passphrase: "sF6EJzpNqKI6pO43lRuXYPb1x/SZlNc22ZR7S2D5kM4=",
     rejectUnauthorized: false 
-};
+}
 
-const logger = winston.createLogger({
-    level: "info", // Set log level
-    format: winston.format.json(), // Use JSON format
-    defaultMeta: { service: "node-http2-app" }, // Add metadata
-    transports: [
-        new winston.transports.Console(), // Log to console
-    ],
-});
-
-router.get("/admin", async (req, res) => {  
-    try {
-        res.json({ message: "Welcome to HTTP2 API" });
-        logger.info("Request handled successfully");
-    } catch (error) {
-        logger.error("Error in /admin route", { error });
-        res.status(500).json({ error: "Internal Server Error" });
+spdy
+  .createServer(options, app)
+  .listen(port, (error) => {
+    if (error) {
+      console.error(error)
+    } else {
+      console.log('Listening on port: ' + port + '.')
     }
-});
-
-router.get("/users", (req, res) => {
-    try {
-        res.json(users);
-    } catch (error) {
-        console.error("Error in /users route:", error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-});
-
-app.use('/api', router);
-
-const server = http2.createSecureServer(options, app)
-
-server.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
-    console.log(`SSL ${useSSL ? 'enabled' : 'disabled'}`);
-});
+  })
