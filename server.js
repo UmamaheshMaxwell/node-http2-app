35b6a2928033684e18d0ad66c3d8dc1fbf26e58d
Umamaheswararao Meka <maheshmeka16@gmail.com>
2023-09-01 12:55:27 +0000

    added user end points

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http2 = require('http2');
-
+const fs = require("fs")
 const PORT = process.env.PORT || 8080;
 
 const server = http2.createServer();
@@ -16,7 +16,27 @@ server.on('stream', (stream, headers) => {
     // Handle the /about endpoint
     stream.respond({ ':status': 200, 'content-type': 'text/plain' });
     stream.end('This is the about page.');
-  } else {
+  } else if (method === 'GET' && path === '/users') {
+    // Read the JSON data from a file (users.json)
+    fs.readFile('users.json', 'utf8', (err, data) => {
+      if (err) {
+        // Handle error, e.g., file not found
+        stream.respond({ ':status': 500, 'content-type': 'text/plain' });
+        stream.end('Internal Server Error', err);
+        return;
+      }
+
+      // Set response headers
+      stream.respond({
+        ':status': 200,
+        'content-type': 'application/json',
+      });
+
+      // Send the JSON data as a string
+      stream.end(data);
+    })
+  }
+  else {
     // Handle other endpoints or return a 404
     stream.respond({ ':status': 404, 'content-type': 'text/plain' });
     stream.end('Not Found');
