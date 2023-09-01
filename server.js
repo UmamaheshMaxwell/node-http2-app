c0cd8809429031061b707411b7fb275181bd2c0a
Umamaheswararao Meka <maheshmeka16@gmail.com>
2023-09-01 12:37:48 +0000

    added end points

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-const http2 = require("http2")
-const os = require("os")
+const http2 = require('http2');
 
+const PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 8080
-const addr = `0.0.0.0:${PORT}`
+const server = http2.createServer();
 
+server.on('stream', (stream, headers) => {
+  const method = headers[':method'];
+  const path = headers[':path'];
 
-const server = http2.createServer()
+  if (method === 'GET' && path === '/') {
+    // Handle the root endpoint
+    stream.respond({ ':status': 200, 'content-type': 'text/plain' });
+    stream.end('Hello, this is the root endpoint!');
+  } else if (method === 'GET' && path === '/about') {
+    // Handle the /about endpoint
+    stream.respond({ ':status': 200, 'content-type': 'text/plain' });
+    stream.end('This is the about page.');
+  } else {
+    // Handle other endpoints or return a 404
+    stream.respond({ ':status': 404, 'content-type': 'text/plain' });
+    stream.end('Not Found');
+  }
+});
 
-server.on("stream", (stream, headers) => {
-  stream.respond({
-    'content-type': 'text/plain',
-    ':status': 200
-  })
-  console.log(headers)
-  stream.end(`This requested is served over HTTP2 protocol using NodeJS`)
-})
-
-server.listen(PORT, ()=>{
-  console.log(`Server is listening at ${PORT}`)
-})
-
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
