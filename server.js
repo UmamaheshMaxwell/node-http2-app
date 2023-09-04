2b2654007c9f800d455b350f7cadf571e852a1b4
UmamaheshMaxwell <devopswithkube@gmail.com>
2023-09-04 10:31:29 +0530

    back to http2 package

--- server.js
+++ server.js
@@ -5,44 +5,31 @@ const PORT = process.env.PORT || 8080;
 const server = http2.createServer();
 
 server.on('stream', (stream, headers) => {
-  const method = headers[':method'];
-  const path = headers[':path'];
+  const method = headers[':method']
+  const path = headers[':path']
 
   if (method === 'GET' && path === '/') {
-    // Handle the root endpoint
     stream.respond({ ':status': 200, 'content-type': 'text/plain' });
-    stream.end('Hello, this is the root endpoint!');
-  } else if (method === 'GET' && path === '/about') {
-    // Handle the /about endpoint
-    stream.respond({ ':status': 200, 'content-type': 'text/plain' });
-    stream.end('This is the about page.');
+    stream.end('Hello, Welcome to HTTP2 Endpoint')
   } else if (method === 'GET' && path === '/users') {
-    // Read the JSON data from a file (users.json)
     fs.readFile('users.json', 'utf8', (err, data) => {
       if (err) {
         // Handle error, e.g., file not found
         stream.respond({ ':status': 500, 'content-type': 'text/plain' });
-        stream.end('Internal Server Error', err);
+        stream.end('Internal Server Error', err)
         return;
       }
-
-      // Set response headers
-      stream.respond({
-        ':status': 200,
-        'content-type': 'application/json',
-      });
-
-      // Send the JSON data as a string
-      stream.end(data);
+      stream.respond({':status': 200,'content-type': 'application/json',})
+      stream.end(data)
     })
   }
   else {
     // Handle other endpoints or return a 404
     stream.respond({ ':status': 404, 'content-type': 'text/plain' });
-    stream.end('Not Found');
+    stream.end('Not Found')
   }
 });
 
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
-});
+})
