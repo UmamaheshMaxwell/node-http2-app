69e3e60e60c415e8034c0b1e25780fa9350f3f59
UmamaheshMaxwell <devopswithkube@gmail.com>
2023-09-05 21:57:34 +0530

    added hapi server

---
+++ routes/handleRequest.js
@@ -0,0 +1,21 @@
+function handleRequest(stream, method, path) {
+    if (method === 'GET' && path === '/') {
+      stream.respond({ ':status': 200, 'content-type': 'text/plain' });
+      stream.end('Hello, Welcome to HTTP2 Endpoint');
+    } else if (method === 'GET' && path === '/users') {
+      fs.readFile('users.json', 'utf8', (err, data) => {
+        if (err) {
+          stream.respond({ ':status': 500, 'content-type': 'text/plain' });
+          stream.end('Internal Server Error', err);
+        } else {
+          stream.respond({ ':status': 200, 'content-type': 'application/json' });
+          stream.end(data);
+        }
+      });
+    } else {
+      stream.respond({ ':status': 404, 'content-type': 'text/plain' });
+      stream.end('Not Found');
+    }
+  }
+
+  module.exports = handleRequest
--- server.js
+++ server.js
@@ -1,35 +1,57 @@
-const http2 = require('http2');
-const fs = require("fs")
-const PORT = process.env.PORT || 8080;
+// const http2 = require('http2');
+// const fs = require('fs');
+// const handleRequest = require("./routes/handleRequest")
 
-const server = http2.createServer();
+// const PORT = process.env.PORT || 8080;
 
-server.on('stream', (stream, headers) => {
-  const method = headers[':method']
-  const path = headers[':path']
+// const server = http2.createServer();
 
-  if (method === 'GET' && path === '/') {
-    stream.respond({ ':status': 200, 'content-type': 'text/plain' });
-    stream.end('Hello, Welcome to HTTP2 Endpoint')
-  } else if (method === 'GET' && path === '/users') {
-    fs.readFile('users.json', 'utf8', (err, data) => {
-      if (err) {
-        // Handle error, e.g., file not found
-        stream.respond({ ':status': 500, 'content-type': 'text/plain' });
-        stream.end('Internal Server Error', err)
-        return;
-      }
-      stream.respond({':status': 200,'content-type': 'application/json',})
-      stream.end(data)
-    })
-  }
-  else {
-    // Handle other endpoints or return a 404
-    stream.respond({ ':status': 404, 'content-type': 'text/plain' });
-    stream.end('Not Found')
+// server.on('stream', (stream, headers) => {
+//   const method = headers[':method'];
+//   const path = headers[':path'];
+//   handleRequest(stream, method, path);
+// });
+
+// server.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}`);
+// });
+
+var fs = require('fs');
+var Hapi = require('@hapi/hapi');
+var http2 = require('http2');
+
+
+var server = new Hapi.Server({
+  listener: http2.createServer(),
+ // host: 'localhost',
+  port: 8080
+});
+
+server.route({
+    method: 'GET',
+    path:'/', 
+    handler: function (request, reply) {
+        return 'Welcome to HTTP2 Server';
+    }
+});
+
+server.route({
+  method: 'GET',
+  path:'/users', 
+  handler: function (request, reply) {
+      return fs.readFileSync('users.json', 'utf8')
   }
 });
 
-server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-})
+async function startServer() {
+  try {
+    await server.start();
+    console.log(server.info);
+    console.log(`Server started at ${server.info.uri}`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+startServer();
