30de8c173c078bc015aa9fdb15456b0e171e7f2f
UmamaheshMaxwell <devopswithkube@gmail.com>
2023-09-06 00:59:05 +0530

    code cleanup

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const http2 = require('http2');
-const handleRequest = require("./routes/handleRequest")
-
+const fs = require("fs")
 const PORT = process.env.PORT || 8080;
 
 const server = http2.createServer();
@@ -8,12 +7,62 @@ const server = http2.createServer();
 server.on('stream', (stream, headers) => {
   const method = headers[':method'];
   const path = headers[':path'];
-  handleRequest(stream, method, path);
+
+  if (method === 'GET' && path === '/') {
+    // Handle the root endpoint
+    stream.respond({ ':status': 200, 'content-type': 'text/plain' });
+    stream.end('Hello, this is the root endpoint!');
+  } else if (method === 'GET' && path === '/about') {
+    // Handle the /about endpoint
+    stream.respond({ ':status': 200, 'content-type': 'text/plain' });
+    stream.end('This is the about page.');
+  } else if (method === 'GET' && path === '/users') {
+    // Read the JSON data from a file (users.json)
+    fs.readFile('users.json', 'utf8', (err, data) => {
+      if (err) {
+        // Handle error, e.g., file not found
+        stream.respond({ ':status': 500, 'content-type': 'text/plain' });
+        stream.end('Internal Server Error', err);
+        return;
+      }
+
+      // Set response headers
+      stream.respond({
+        ':status': 200,
+        'content-type': 'application/json',
+      });
+
+      // Send the JSON data as a string
+      stream.end(data);
+    })
+  }
+  else {
+    // Handle other endpoints or return a 404
+    stream.respond({ ':status': 404, 'content-type': 'text/plain' });
+    stream.end('Not Found');
+  }
 });
 
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
+// const http2 = require('http2');
+// const handleRequest = require("./routes/handleRequest")
+
+// const PORT = process.env.PORT || 8080;
+
+// const server = http2.createServer();
+
+// server.on('stream', (stream, headers) => {
+//   const method = headers[':method'];
+//   const path = headers[':path'];
+//   handleRequest(stream, method, path);
+// });
+
+// server.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}`);
+// });
 
 // var fs = require('fs');
 // var Hapi = require('@hapi/hapi');
